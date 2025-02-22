a44c272c646ea83ae985316377e10f3e14c0f4ae
simret <simrettony@gmail.com>
2025-02-22 17:30:35 +0100

    new file is uploaded

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  #default = "+++"
+}
+
+variable "git-token" {
+  #default = "xxxxxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  #default = "+++"
+}
+
+variable "hosted-zone" {
+  #default = "++++++"
+}
